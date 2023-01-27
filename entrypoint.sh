5cc080d286b200beb51866e8fd90f783168cd9d2
Tommy Heyding <toh@hbt.de>
2023-01-27 12:58:51 +0100

    Update echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /iris-main -a /opt/irisbuild/do-conversion.sh -l /usr/irissys/mgr/messages.log --check-caps false --ISCAgent false
-echo "Entrypoint exit"
+echo "[INFO] Stop container..."
